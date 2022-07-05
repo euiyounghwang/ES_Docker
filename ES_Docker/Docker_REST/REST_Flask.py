fd46f122ce35a0b08570b8dc1dc5254e83e3b41d
euiyoung.hwang <marieuig@gmail.com>
2022-07-05 13:03:27 -0500

    dockerfile update

--- ES_Docker/Docker_REST/REST_Flask.py
+++ ES_Docker/Docker_REST/REST_Flask.py
@@ -1,14 +1,16 @@
 
 # https://betterprogramming.pub/create-a-running-docker-container-with-gunicorn-and-flask-dcd98fddb8e0
 # gunicorn REST_Flask:app -w 2 --threads 2 -b 0.0.0.0:8000
+# pigar -p ./requirements.txt - P ./
 
 import datetime
 from flask import Flask
 import sys
 
-sys.path.append('/Users/euiyoung.hwang/ES/Python_Workspace/')
+# sys.path.append('/Users/euiyoung.hwang/ES/Python_Workspace/')
+# import ES_Docker.Docker_REST.lib.Utils.Util as Util
 
-import ES_Docker.Docker_REST.lib.Utils.Util as Util
+import lib.Utils.Util as Util
 
 app = Flask(__name__)
 
