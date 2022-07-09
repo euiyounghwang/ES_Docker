413dbf7f0f52514b6562bee7d2a39901bc865ca3
euiyoung.hwang <marieuig@gmail.com>
2022-07-09 00:13:13 -0500

    dockerfile build & run

--- ES_Docker/Docker_RESTful/Fast_Class2_API.py
+++ ES_Docker/Docker_RESTful/Fast_Class2_API.py
@@ -28,9 +28,9 @@ from lib.Logstash_IF.Logstash_Socket import *
 class Logstashs:
     def __init__(self, message):
         self.TCP_SOC = None
-        self.SOCKET_SERVER_IP = '127.0.0.1'
+        # self.SOCKET_SERVER_IP = '127.0.0.1'
         # Internal docker connect to external host
-        # self.SOCKET_SERVER_IP = 'host.docker.internal'
+        self.SOCKET_SERVER_IP = 'host.docker.internal'
         self.TCP_SOC = TCP_SOCKET(self.SOCKET_SERVER_IP, 5958)
         self.TCP_SOC.Connect()
         self.message = message
@@ -51,7 +51,8 @@ async def create_item(item: Item):
         print('json - > ', item.json())
         json_results = jsonable_encoder(item)
         Logstashs(json_results).send_socket_msg()
-        return JSONResponse(content=json_results)
+        # return JSONResponse(content=json_results)
+        return dict(json_results)
     finally:
         pass
 
