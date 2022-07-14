7ab0cb8931cf7897945645d7b807a00b91b07c45
euiyoung.hwang <marieuig@gmail.com>
2022-07-14 08:17:30 -0500

    dockerfile build & run

---
+++ ES_Docker/Docker_RESTful/Fast_API_WebSocket.py
@@ -0,0 +1,71 @@
+
+
+import uvicorn
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:1238/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+
+
+if __name__ == "__main__":
+    """
+    uvicorn main:app --reload
+    # Python Main
+    uvicorn.run(app="main:app", 
+                host="0.0.0.0",
+                port=80,
+                reload=True,
+                workers=4) 
+
+    # Command                
+    uvicorn main:app --reload --host=0.0.0.0 --port=80 --workers=4
+    """
+    uvicorn.run("Fast_API_WebSocket:app", host="127.0.0.1", port=1238, reload=True)
