395a7ca48dffab724728025e4f4f659a69138982
euiyounghwang <marieuig@gmail.com>
2023-08-05 13:42:06 -0500

    Add airflow

---
+++ Airflow/airflow.sh
@@ -0,0 +1,13 @@
+
+# *******
+# airflow
+# https://velog.io/@jenori_dev/airflow-%EB%A7%A5-%ED%84%B0%EB%AF%B8%EB%84%90%EC%97%90%EC%84%9C-%EC%97%90%EC%96%B4%ED%94%8C%EB%A1%9C%EC%9A%B0-%EC%84%A4%EC%B9%98%ED%95%98%EA%B8%B0
+conda create -n [가상환경이름]_env python=3.7
+source activate [가상환경이름]_env
+pip3 install apache-airflow
+airflow users create -u admin -p admin -f euiyoung -l hwang -r Admin -e admin@admin.com
+airflow db init
+#/Users/euiyoung.hwang/airflow (cd ~/airflow)
+airflow webserver -p 8889
+#(admin/admin)
+# *******
